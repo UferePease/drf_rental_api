2719a02990bb47eb6ec96c930d8995d7bd4e3f41
peaceville <uferepeace@gmail.com>
2019-07-16 22:55:59 +0100

    initial commit after project setup

---
+++ drf_rentals/rental/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from drf_rentals import settings
+
+# Create your models here.
+
+class OwnedModel(models.Model):
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+    on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+
+class Friend(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Belonging(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class Borrowed(models.Model):
+    what = models.ForeignKey(Belonging, on_delete=models.CASCADE)
+    to_who = models.ForeignKey(Friend, on_delete=models.CASCADE)
+    when = models.DateTimeField(auto_now_add=True)
+    returned = models.DateTimeField(null=True, blank=True)
